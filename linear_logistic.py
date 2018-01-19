4407b5156d0b9ef07d6b3e0120496aa4ee31d0bc
Yashwant Nagarjuna <ranbonagarjuna@gmail.com>
2018-01-19 23:39:20 +0530

    Lin Reg

--- linear_logistic.py
+++ linear_logistic.py
@@ -7,3 +7,5 @@ book = xlrd.open_workbook(DATA_FILE, encoding_override='utf-8')
 sheet = book.sheet_by_index(0)
 data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
 n_samples = sheet.nrows - 1
+
+# insert placeholders
